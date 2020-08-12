afabaa0e30f3070ea57b6f6d7ef7314f0eace084
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-08-12 07:57:14 -0300

    Intent: MainActivity para ListaUsuariosActivity.

--- app/src/main/java/br/pro/aguiar/ep3fda2/activity/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/ep3fda2/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.pro.aguiar.ep3fda2.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,11 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnLancarDado.visibility = View.GONE
+//        btnLancarDado.visibility = View.GONE
+
+        // Classe Usuario
+        // var user = Usuario()
+
+        btnLancarDado.setOnClickListener {
+            var intentListaUsuarios = Intent(
+                    this,
+                    ListaUsuariosActivity::class.java
+                ) // Intent
+            startActivity(intentListaUsuarios)
+        }
+
     }
 
-    fun actionClickTipoDados(v: View){
-        when (v.id){
+    fun actionClickTipoDados(minhaView: View){
+        when (minhaView.id){
             R.id.btnD4 -> tipo = 4
             R.id.btnD6 -> tipo = 6
             R.id.btnD10 -> tipo = 10
