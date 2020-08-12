bb9f612232bf121de55156b1d79fc20dfebe70b1
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-08-12 08:29:24 -0300

    Intent: Transição entre 3 activities.

--- app/src/main/java/br/pro/aguiar/ep3fda2/activity/ListaUsuariosActivity.kt
+++ app/src/main/java/br/pro/aguiar/ep3fda2/activity/ListaUsuariosActivity.kt
@@ -1,9 +1,11 @@
 package br.pro.aguiar.ep3fda2.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import br.pro.aguiar.ep3fda2.R
+import kotlinx.android.synthetic.main.activity_lista_usuarios.*
 
 
 class ListaUsuariosActivity : AppCompatActivity() {
@@ -11,5 +13,25 @@ class ListaUsuariosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_usuarios)
+
+        // btnAct1 -> Activity1::class.java
+        btnAct1.setOnClickListener {
+            var intent = Intent(
+                    this, Activity1::class.java
+                )
+            startActivity(intent)
+        }
+
+        // btnAct2 -> Activity2::class.java
+        btnAct2.setOnClickListener {
+            var intent = Intent(
+                this, Activity2::class.java
+            )
+            startActivity(intent)
+        }
     }
+
+//    fun toOtherActivity(){
+//
+//    }
 }
